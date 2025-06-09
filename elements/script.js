a4a17273993c4eaa6e0c324a72e3ced61519dbda
andrej1011 <82315624+andrej1011@users.noreply.github.com>
2025-06-09 18:24:50 +0200

    Add files via upload

---
+++ elements/script.js
@@ -0,0 +1,30 @@
+ function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function sakrijfooter(){
+  var element = document.getElementById('futer');
+  if(element.style.visibility === 'visible')
+    element.style.visibility='hidden';
+  else
+    element.style.visibility='visible';
+}
+
+function promeni_video0(){
+  document.getElementById('ajfrejm').src = "https://www.youtube.com/embed/nnP4LAgJ8C8?start=46";
+}
+function promeni_video1(){
+  document.getElementById('ajfrejm').src = "https://www.youtube.com/embed/fgwLCnHHJNQ";
+}
+function promeni_video2(){
+  document.getElementById('ajfrejm').src = "https://www.youtube.com/embed/yzOFeUDpauU";
+}
+function promeni_video3(){
+  document.getElementById('ajfrejm').src = "https://www.youtube.com/embed/-NK_lD7Grfw?t=42";
+}
+function promeni_video4(){
+  document.getElementById('ajfrejm').src = "https://www.youtube.com/embed/_NQeg4oPjwc";
+}
+
+  
